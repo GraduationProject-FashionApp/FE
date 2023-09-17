0e72d696fdcdbd889290774a4fc35fcacd5829d2
Dongwon Shin <edvedv@naver.com>
2023-09-18 03:31:35 +0900

    Feat: 검색결과 구현

--- LookThat/presentation/src/main/java/com/gradu/lookthat/views/search/SearchResultFragment.kt
+++ LookThat/presentation/src/main/java/com/gradu/lookthat/views/search/SearchResultFragment.kt
@@ -15,17 +15,28 @@ import com.gradu.lookthat.databinding.FragmentSearchResultBinding
 class SearchResultFragment : BaseFragment<FragmentSearchResultBinding>(R.layout.fragment_search_result){
     override fun initView() {
         super.initView()
+        initViewPager()
 
-        // ViewPager2 Adapter 설정
-        val adapter = ViewPagerAdapter(requireActivity())
-        binding.fragmentSearchResultVp.adapter = adapter
-        val tabTitle = listOf<String>("비슷한 옷 보기", "함께 입을 옷 보기")
-        // TabLayout과 ViewPager2 연결
-        TabLayoutMediator(binding.fragmentSearchResultTab, binding.fragmentSearchResultVp) { tab, position ->
-            tab.text = "Tab ${position + 1}"
-        }.attach()
+        // Navigation Args로부터 URI String을 받아옵니다.
+        val args = SearchResultFragmentArgs.fromBundle(requireArguments())
+        val imageUriString = args.imageUri // 가정: Args 이름이 imageUri 입니다.
+        val uri = Uri.parse(imageUriString)
+
+        // Glide를 사용하여 이미지 뷰에 이미지를 로드합니다.
+        Glide.with(this)
+            .load(uri)
+            .into(binding.imageView) // 가정: FragmentSearchResultBinding에 imageView라는 이름의 ImageView가 있습니다.
     }
 
+    private fun initViewPager() {
+        val adapter = ViewPagerAdapter(requireActivity())
+        binding.fragmentSearchResultVp.adapter = adapter
+        val tabTitle = listOf("비슷한 옷 보기", "함께 입을 옷 보기")
+
+        TabLayoutMediator(binding.fragmentSearchResultTab, binding.fragmentSearchResultVp) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
+    }
     inner class ViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
         override fun getItemCount(): Int = 2
 
