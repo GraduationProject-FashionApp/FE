4334321f3c8fb31556bb2e73367307b558433c07
dobby <dobbidd0@gmail.com>
2023-12-05 02:45:42 +0900

    Feat : product detail activity xml

---
+++ LookThat/presentation/src/main/java/com/gradu/lookthat/views/search/SearchProductDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.gradu.lookthat.views.search
+
+import com.gradu.lookthat.base.BaseActivity
+import com.gradu.lookthat.databinding.ActivityProductDetailBinding
+
+class SearchProductDetailActivity : BaseActivity<ActivityProductDetailBinding>(ActivityProductDetailBinding::inflate) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+}
